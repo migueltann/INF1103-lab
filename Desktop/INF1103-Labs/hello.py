92000c2a85023b66d3d45c829d56db9937e411d6
migueltann <miguel_tan@icloud.com>
2026-09-04 11:44:06 +0800

    Dynamic profile & Fun conditions

--- Desktop/INF1103-Labs/hello.py
+++ Desktop/INF1103-Labs/hello.py
@@ -3,19 +3,29 @@ print("Welcome here")
 print("My first post!")
 print("====================")
 
-username = "cool creator"
-bio = "Fun blogger"
-followers = 100
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter content category:")
 
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
-
+print("\n Instagram Profile")
+print("=====================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Content Category:", category)
+
+if age > 40 and category == "fun":
+    print("You are old, what is fun for you?")
+
+
+# followers += 50
+# print("Day 1:", followers)
+
+# followers += 20
+# print("Day 2:", followers)
+
+# followers -= 10
+# print("Day 3:", followers)
+
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
