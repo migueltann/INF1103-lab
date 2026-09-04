b3ff8f1ee22c87da999252206a3832caf092db9a
migueltann <miguel_tan@icloud.com>
2026-09-04 11:31:05 +0800

    Add profile

--- Desktop/INF1103-Labs/hello.py
+++ Desktop/INF1103-Labs/hello.py
@@ -2,3 +2,11 @@ print("====================")
 print("Welcome here")
 print("My first post!")
 print("====================")
+
+username = "cool creator"
+bio = "Fun blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
