aa30de0bcb798ce92ce9c9d34208c723e94b9933
migueltann <miguel_tan@icloud.com>
2026-09-18 15:57:38 +0800

    Added process_delivery function

--- Desktop/INF1103_week1_lab_MiguelTan/modular_auditor.py
+++ Desktop/INF1103_week1_lab_MiguelTan/modular_auditor.py
@@ -13,26 +13,17 @@ def get_valid_input():
     else:
         return int(stock)
 
-while True:
-    stock = get_valid_input()
+def process_delivery(current_total, new_value):
+    current_total += new_value
+    return current_total
 
-    if stock.lower() == "quit":
-        break
 
-    elif not stock.isdigit():
-        print("Error: Please enter a valid positive integer.")
-        failed_entries += 1
-        continue
 
-    else:
-        stock = int(stock)
-        inventory += stock
 
-        print("Current inventory:", inventory)
+
+        
 
         if inventory > 500:
             print("OVERSTOCK ALERT!")
             break
         
-print("Total Units Processed:", inventory)
-print("Number of Failed/Rejected Entries:", failed_entries)
