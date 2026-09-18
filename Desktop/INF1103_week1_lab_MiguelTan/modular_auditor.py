147ef08914a80fb2fe1f7a36ea533d50c5697984
migueltann <miguel_tan@icloud.com>
2026-09-18 15:58:17 +0800

    Added tax and generate report function

--- Desktop/INF1103_week1_lab_MiguelTan/modular_auditor.py
+++ Desktop/INF1103_week1_lab_MiguelTan/modular_auditor.py
@@ -17,7 +17,13 @@ def process_delivery(current_total, new_value):
     current_total += new_value
     return current_total
 
+def calculate_tax(amount):
+    tax = amount * 0.1
+    return tax
 
+def generate_report(total_units, failed_entries):
+    print("Total Units Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_entries)
 
 
 
